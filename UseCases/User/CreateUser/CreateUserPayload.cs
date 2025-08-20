d025fee4707bc5904f5123ff9d9fe7a7ed06d7a3
devYuno <annykessy20@gmail.com>
2025-08-20 16:16:04 -0300

    Created Create User UseCase

--- UseCases/User/CreateUser/CreateUserPayload.cs
+++ UseCases/User/CreateUser/CreateUserPayload.cs
@@ -1,3 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using RPlace.Models;
+
 namespace RPlace.UseCases.User.CreateUser;
 
-public record CreateUserPayload{}
+public record CreateUserPayload
+{
+    [Required]
+    [MinLength(5)]
+    [MaxLength(20)]
+    public string Username { get; init; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; init; }
+
+    [Required]
+    [MinLength(5)]
+    //[NeedNumber]
+    public string Password { get; init; }
+
+    [Required]
+    [Compare("Password")]
+    public string RepeatPassword { get; init; }
+
+    [Url]
+    public string LinkPicture { get; set; }
+
+    [MaxLength(200)]
+    public string Bio { get; init; }
+
+}
--- UseCases/User/CreateUser/CreateUserUseCase.cs
+++ UseCases/User/CreateUser/CreateUserUseCase.cs
@@ -1,11 +1,25 @@
 using RPlace.Models;
+using RPlace.Services.Password;
+using RPlace.Services.Users;
 
 namespace RPlace.UseCases.User.CreateUser;
 
-public record CreateUserUseCase(RPlaceDbContext ctx)
+public class CreateUserUseCase(
+    IUsersService usersService,
+    IPasswordService passwordService
+)
 {
     public async Task<Result<CreateUserResponse>> Do(CreateUserPayload payload)
     {
+        var user = new Player
+        {
+            Bio = payload.Bio,
+            Email = payload.Email,
+            Username = payload.Username,
+            Password = passwordService.Hash(payload.Password)
+
+        };
+
         return Result<CreateUserResponse>.Success(null);
     }
 }
