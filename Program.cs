92181995da091a3b62592b1cd00937eccd752ce3
JulianaStadler <julianastadlerberger@gmail.com>
2025-08-20 14:33:50 -0300

    UseCases configurated as services

--- Program.cs
+++ Program.cs
@@ -23,10 +23,11 @@ using RPlace.UseCases.User.SeeCreatedRooms;
 using RPlace.UseCases.User.SeeInvite;
 using RPlace.UseCases.User.SeePlans;
 using RPlace.UseCases.User.UpdatePlan;
-
-
-
-
+using RPlace.Services.JWT;
+using RPlace.Services.Password;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<RPlaceDbContext>(options => {
@@ -34,6 +35,58 @@ builder.Services.AddDbContext<RPlaceDbContext>(options => {
     options.UseSqlServer(sqlConn);
 });
 
+
+var jwtSecret = Environment.GetEnvironmentVariable("JWT_SECRET");
+var keyBytes = Encoding.UTF8.GetBytes(jwtSecret);
+var key = new SymmetricSecurityKey(keyBytes);
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidIssuer = "RPlace",
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero,
+            IssuerSigningKey = key,
+        };
+    });
+
+builder.Services.AddTransient<IPasswordService, PBKDF2PasswordService>();
+builder.Services.AddSingleton<IJWTService, JWTService>();
+// builder.Services.AddTransient<IProfilesService, EFProfileService>();
+
+builder.Services.AddTransient<ChangePermissionUseCase>();
+builder.Services.AddTransient<InviteUserUseCase>();
+builder.Services.AddTransient<PaintPixelUseCase>();
+builder.Services.AddTransient<RemoveUserUseCase>();
+builder.Services.AddTransient<SeePixelsUseCase>();
+builder.Services.AddTransient<SeePlayersUseCase>();
+builder.Services.AddScoped<SearchUserUseCase>();
+builder.Services.AddScoped<SeeUserUseCase>();
+builder.Services.AddTransient<AnswerInviteUseCase>();
+builder.Services.AddTransient<CreateRoomUseCase>();
+builder.Services.AddTransient<CreateUserUseCase>();
+builder.Services.AddTransient<DeleteAccountUseCase>();
+builder.Services.AddTransient<EditAccountUseCase>();
+builder.Services.AddTransient<LoginUseCase>();
+builder.Services.AddTransient<LogoutUseCase>();
+builder.Services.AddScoped<SeeCreatedRoomsUseCase>();
+builder.Services.AddScoped<SeeInviteUseCase>();
+builder.Services.AddScoped<SeePlansUseCase>();
+builder.Services.AddTransient<UpdatePlanUseCase>();
+
+
+
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+
+
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
