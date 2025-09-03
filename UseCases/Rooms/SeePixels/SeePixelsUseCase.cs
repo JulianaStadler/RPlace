3a5539f44f3fb85534730b131b1d0b3ece1eec9c
JulianaStadler <julianastadlerberger@gmail.com>
2025-09-03 16:06:10 -0300

    a

--- UseCases/Rooms/SeePixels/SeePixelsUseCase.cs
+++ UseCases/Rooms/SeePixels/SeePixelsUseCase.cs
@@ -1,11 +1,23 @@
 using RPlace.Models;
+using RPlace.Services.Rooms;
+using RPlace.Services.Users;
+
 
 namespace RPlace.UseCases.Rooms.SeePixels;
 
-public record SeePixelsUseCase(RPlaceDbContext ctx)
+public record SeePixelsUseCase(RPlaceDbContext ctx, EFRoomService roomService)
 {
     public async Task<Result<SeePixelsResponse>> Do(SeePixelsPayload payload)
     {
+        var roomIdService = await roomService.FindById(id);
+            if (roomIdService == null) 
+                return Results.NotFound("Room not found");
+
+            var roomId = roomIdService.Id;
+            var userIdClaim = http.User.FindFirst(ClaimTypes.NameIdentifier);
+            var userId = userIdClaim != null ? Guid.Parse(userIdClaim.Value) : Guid.Empty;
+
+
         return Result<SeePixelsResponse>.Success(null);
     }
 }
